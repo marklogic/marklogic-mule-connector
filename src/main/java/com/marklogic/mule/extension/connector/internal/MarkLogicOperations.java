d3420fe5e7f5956d94638ad23b3808feab0fa2d9
Jonathan Krebs <jkrebs@marklogic.com>
2018-09-11 16:44:39 -0400

    Added first stab at changes to support writing JSON streams to MarkLogic

--- src/main/java/com/marklogic/mule/extension/connector/internal/MarkLogicOperations.java
+++ src/main/java/com/marklogic/mule/extension/connector/internal/MarkLogicOperations.java
@@ -1,13 +1,17 @@
 package com.marklogic.mule.extension.connector.internal;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.UUID;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.nio.charset.Charset;
+
+import java.util.*;
+
 import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
@@ -33,6 +37,11 @@ import org.mule.runtime.extension.api.annotation.param.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 /* This class is a container for operations, every public method in this class will be taken as an extension operation. */
 public class MarkLogicOperations
@@ -43,14 +52,15 @@ public class MarkLogicOperations
     // Loading files into MarkLogic asynchronously InputStream docPayload
   @MediaType(value = APPLICATION_JSON, strict = true)
   public String importDocs(@Config MarkLogicConfiguration configuration, @Connection MarkLogicConnection connection, InputStream docPayloads, String basenameUri) {
-        
-        /* Grab the input docPayload Object array that Mule Batch sends and convert it to a String array */
-        InputStreamHandle handle = new InputStreamHandle(docPayloads);
-        System.out.println(handle.toString());
-        
+
+        System.out.println("uri list: " + basenameUri);
+
+        // Parse the string representation of the list of base uris into a List of base uri strings
+        List<String> uris = parseUriListString(basenameUri);
+
         DocumentMetadataHandle metah = new DocumentMetadataHandle();
 
-        // Collections, quality, and permissions.  
+        // Collections, quality, and permissions.
         // Permissions are additive to the rest-reader,read and rest-writer,update.
         String[] configCollections = configuration.getOutputCollections();
         if (!configCollections[0].equals("null")) {
@@ -81,17 +91,9 @@ public class MarkLogicOperations
                     System.out.println("No additive permissions assigned");
             }
         }
-        
-        // determine output URI
-        String outURI;
-        if (basenameUri.length() > 0 && configuration.getGenerateOutputUriBasename() == Boolean.FALSE) {
-            outURI = configuration.getOutputPrefix() + basenameUri + configuration.getOutputSuffix();
-        } else {
-            String uuid = UUID.randomUUID().toString();
-            outURI = configuration.getOutputPrefix() + uuid + configuration.getOutputSuffix();
-        }
-        
-        // create and configure the job
+
+        // Parse the given InputStream into a List of documents
+        List documents = parseObjectStream(docPayloads);
         DatabaseClient myClient = connection.getClient();
         DataMovementManager dmm = myClient.newDataMovementManager();
         WriteBatcher batcher = dmm.newWriteBatcher();
@@ -106,7 +108,7 @@ public class MarkLogicOperations
         });
         
         String configTransform = configuration.getServerTransform();
-        if (configTransform.equals("null")) {
+        if ((configTransform == null) || (configTransform.equals("null"))) {
             System.out.println("Ingesting doc payload without a transform");
         } else {
             ServerTransform thistransform = new ServerTransform(configTransform);
@@ -121,11 +123,36 @@ public class MarkLogicOperations
             batcher.withTransform(thistransform);
             System.out.println("Transforming input doc payload with transform: " + thistransform.getName());
         }
-        
+
+        // Create a template into which to put the base URI
+        String uriTemplate = configuration.getOutputPrefix() + "%s" + configuration.getOutputSuffix();
+
         // start the job and feed input to the batcher
         JobTicket jt = dmm.startJob(batcher);
         try {
-            batcher.add(outURI, metah, handle);
+            for (int i = 0; i < documents.size(); i++) {
+                // determine output URI for the current document
+                String baseURI;
+                String outURI;
+                // If the config tells us to generate a new URI, just do that
+                if (configuration.getGenerateOutputUriBasename() == Boolean.TRUE) {
+                    baseURI = UUID.randomUUID().toString();
+                } else {
+                    // If we've moved past the end of the URI array, generate a new one
+                    if (i >= uris.size()) {
+                        baseURI = UUID.randomUUID().toString();
+                    } else {
+                        // Use the base URI, unless it's null or "",  then generate one
+                        baseURI = uris.get(i);
+                        if ((baseURI == null) || (baseURI.length() < 1)) {
+                            baseURI = UUID.randomUUID().toString();
+                        }
+                    }
+                }
+                outURI = String.format(uriTemplate,baseURI);
+                System.out.println("outURI: " + outURI);
+                batcher.addAs(outURI,metah,documents.get(i));
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -296,6 +323,168 @@ public class MarkLogicOperations
             //TODO: Figure out if result of "run" was actually successful 
             return "Success";
         }
+    }
+    /**
+     *
+     * @param uriListString : a list of URIs surrounded by [] and separated by commas
+     * @return
+     */
+    private List<String> parseUriListString(String uriListString) {
+        List<String> retVal;
+        // If the uri list string is null or "", just return a new (empty) List
+        if ((uriListString == null) || (uriListString.length() == 0)) {
+            retVal = new ArrayList<String>();
+        } else {
+            // trim the [ and ] off the list string
+            String trimmedList = uriListString.substring(1, uriListString.length() - 1);
+            // parse the string by commas
+            StringTokenizer parser = new StringTokenizer(trimmedList, ",");
+            // count the number of items in the list
+            int listLength = parser.countTokens();
+            retVal = new ArrayList<String>(listLength);
+            // if the list has no items, that means there are no commas. just add that to the list and bail.
+            if (listLength == 0) {
+                retVal.add(trimmedList.trim());
+            } else {
+                // otherwise, walk thru all the items and add them to the list
+                while (parser.hasMoreElements()) {
+                    retVal.add(parser.nextToken().trim());
+                }
+            }
+        }
+        return retVal;
+    }
+    private List parseObjectStream(InputStream stream) {
+        List documents = new ArrayList();
+
+        try {
+            // Create an ObjectMapper/parser to pull JSON objects out of the given stream
+            ObjectMapper mapper = new ObjectMapper();
+            JsonParser parser = mapper.getFactory().createParser(stream);
+
+            // If we've gotten this far without an exception, it means we have JSON
+            System.out.println("Treating stream as json");
+
+            // Make sure the first token is the start of a JSON array.
+            JsonToken firstToken = parser.nextToken();
+
+            if (firstToken != JsonToken.START_ARRAY && firstToken != JsonToken.START_OBJECT) {
+                throw new IllegalStateException("Expected an array or object, but got: " + firstToken);
+            }
+            if (parser.nextToken() == JsonToken.START_OBJECT) {
+                ObjectNode node = mapper.readTree(parser);
+                documents.add(node.toString());
+                System.out.println("adding: " + node.toString());
+            }
+            while (parser.nextToken() == JsonToken.START_OBJECT) {
+                // read everything from this START_OBJECT to the matching END_OBJECT
+                // and return it as a tree model ObjectNode
+                ObjectNode node = mapper.readTree(parser);
+                documents.add(node.toString());
+                System.out.println("adding: " + node.toString());
+            }
+
+            parser.close();
+
+        } catch (JsonProcessingException e) {
+            try {
+                SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
+                saxParser.parse(stream,new DefaultHandler());
+                System.out.println("Treating content as XML");
+            } catch (SAXException e1) {
+                System.out.println("Treating content as Binary");
+                documents = new ArrayList();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return documents;
+    }
+    /**
+     * Unit tests
+     * @param args sent in on the command line
+     */
+    public static void main(String[] args) {
+
+        MarkLogicOperations ops = new MarkLogicOperations();
+        System.out.println("parseUriListString TEST 1.  Results should show 5 numbers with no spaces");
+        String uriListString = "[10004, 10002, 10003, 10001, 10005]";
+        for (String uri : ops.parseUriListString(uriListString))
+            System.out.println("uri: |" + uri + "|");
+        System.out.println("parseUriListString TEST 2.  Results should show 1 number with no spaces");
+        uriListString = "[10004]";
+        for (String uri : ops.parseUriListString(uriListString))
+            System.out.println("uri: |" + uri + "|");
+        System.out.println("parseUriListString TEST 3.  Shouldn't return anything");
+        uriListString = "";
+        for (String uri : ops.parseUriListString(uriListString))
+            System.out.println("uri: |" + uri + "|");
+        System.out.println("parseUriListString TEST 4.  Shouldn't return anything");
+        uriListString = null;
+        for (String uri : ops.parseUriListString(uriListString))
+            System.out.println("uri: |" + uri + "|");
+        String json = "[\n" +
+                "  {\n" +
+                "    \"employee\": {\n" +
+                "      \"employeeNumber\": \"10004\",\n" +
+                "      \"hireDate\": \"1986-12-01T00:00:00\",\n" +
+                "      \"firstName\": \"Chirstian\",\n" +
+                "      \"lastName\": \"Koblick\",\n" +
+                "      \"birthDate\": \"1954-05-01T00:00:00\",\n" +
+                "      \"gender\": \"M\",\n" +
+                "      \"extractedDateTime\": \"2018-09-11T10:45:03.232-04:00\"\n" +
+                "    }\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"employee\": {\n" +
+                "      \"employeeNumber\": \"10005\",\n" +
+                "      \"hireDate\": \"1989-09-12T00:00:00\",\n" +
+                "      \"firstName\": \"Kyoichi\",\n" +
+                "      \"lastName\": \"Maliniak\",\n" +
+                "      \"birthDate\": \"1955-01-21T00:00:00\",\n" +
+                "      \"gender\": \"M\",\n" +
+                "      \"extractedDateTime\": \"2018-09-11T10:45:03.232-04:00\"\n" +
+                "    }\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"employee\": {\n" +
+                "      \"employeeNumber\": \"10003\",\n" +
+                "      \"hireDate\": \"1986-08-28T00:00:00\",\n" +
+                "      \"firstName\": \"Parto\",\n" +
+                "      \"lastName\": \"Bamford\",\n" +
+                "      \"birthDate\": \"1959-12-03T00:00:00\",\n" +
+                "      \"gender\": \"M\",\n" +
+                "      \"extractedDateTime\": \"2018-09-11T10:45:03.232-04:00\"\n" +
+                "    }\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"employee\": {\n" +
+                "      \"employeeNumber\": \"10002\",\n" +
+                "      \"hireDate\": \"1985-11-21T00:00:00\",\n" +
+                "      \"firstName\": \"Bezalel\",\n" +
+                "      \"lastName\": \"Simmel\",\n" +
+                "      \"birthDate\": \"1964-06-02T00:00:00\",\n" +
+                "      \"gender\": \"F\",\n" +
+                "      \"extractedDateTime\": \"2018-09-11T10:45:03.232-04:00\"\n" +
+                "    }\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"employee\": {\n" +
+                "      \"employeeNumber\": \"10001\",\n" +
+                "      \"hireDate\": \"1986-06-26T00:00:00\",\n" +
+                "      \"firstName\": \"Georgi\",\n" +
+                "      \"lastName\": \"Facello\",\n" +
+                "      \"birthDate\": \"1953-09-02T00:00:00\",\n" +
+                "      \"gender\": \"M\",\n" +
+                "      \"extractedDateTime\": \"2018-09-11T10:45:03.232-04:00\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "]";
+        // This should return a List of 5 employees
+        System.out.println(ops.parseObjectStream(new ByteArrayInputStream(Charset.forName("UTF-16").encode(json).array())));
+        String xml = "";
     }
 
 }
