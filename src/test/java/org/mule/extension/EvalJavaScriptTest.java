b2eb0d38672110d579866f27af71d434c36ecd77
Phil Barber <Phil.Barber@marklogic.com>
2023-11-02 21:03:34 -0400

    DEVEXP-622: Can eval a JS script on the ML server

---
+++ src/test/java/org/mule/extension/EvalJavaScriptTest.java
@@ -0,0 +1,36 @@
+package org.mule.extension;
+
+import org.junit.Test;
+import org.mule.runtime.api.message.Message;
+
+import static org.junit.Assert.*;
+
+public class EvalJavaScriptTest extends AbstractFlowTester {
+
+    @Override
+    protected String getConfigFile() {
+        return "eval-javascript.xml";
+    }
+
+    @Test
+    public void simplestEvalTest() {
+        Message message = runFlowGetMessage("simple-math");
+        assertEquals("3", message.getPayload().getValue());
+    }
+
+    @Test
+    public void deleteDocumentEvalTest() {
+        runFlowGetMessage("writeDocument");
+        Message message = runFlowGetMessage("readDocument");
+        assertEquals("{\"Hello\":\"World!\"}", message.getPayload().getValue().toString());
+
+        runFlowGetMessage("deleteDocument");
+
+        try {
+            runFlowGetMessage("readDocument");
+            fail("This message should cause the exception because the document should not exist any longer.");
+        } catch (RuntimeException e) {
+            assertTrue(e.getMessage().contains("Local message: Could not read non-existent document."));
+        }
+    }
+}
