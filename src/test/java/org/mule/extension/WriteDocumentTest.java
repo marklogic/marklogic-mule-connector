43caffbe93288ff4e49dde1af4af104ae0f1738e
Rob Rudin <rudin@progress.com>
2023-11-04 09:02:19 -0400

    Restoring WriteDocumentTest
    We don't want the test in it anymore, but this needs to be here so Anushree's PR can be easily merged in.

---
+++ src/test/java/org/mule/extension/WriteDocumentTest.java
@@ -0,0 +1,16 @@
+package org.mule.extension;
+
+import org.junit.Test;
+
+public class WriteDocumentTest extends AbstractFlowTester {
+
+    @Override
+    protected String getConfigFile() {
+        return "write-document.xml";
+    }
+
+    @Test
+    public void test() {
+        // TODO Anushree - remove this after merging your PR in.
+    }
+}
